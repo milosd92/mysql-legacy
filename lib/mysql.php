f281f16e35acb0301d0d7116a6af82a0c17bf26d
milosd92 <milosdanilov@gmail.com>
2016-06-04 19:05:47 +0200

    Added a first procedural function: mysql_connect

--- lib/mysql.php
+++ lib/mysql.php
@@ -7,14 +7,17 @@
  */
 
 /**
+ * Open a connection to a MySQL Server.
+ *
  * @param $server
  * @param $username
  * @param $password
  * @param bool $new_link
  * @param int $client_flags
+ *
  * @return \Danilov\Legacy\MySQL
  */
 function mysql_connect($server, $username, $password, $new_link = false, $client_flags = 0)
 {
-    return new \Danilov\Legacy\MySQL($server, $username, $password, $new_link, $client_flags);
+    return \Danilov\Legacy\MySQL::getConnection($server, $username, $password, $new_link, $client_flags);
 }
