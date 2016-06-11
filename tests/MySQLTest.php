09a6d0c5a858271c3cef1540d9e7d5912c5198fe
milosd92 <milosdanilov@gmail.com>
2016-06-11 16:03:56 +0200

    Added few new tests

--- tests/MySQLTest.php
+++ tests/MySQLTest.php
@@ -2,6 +2,34 @@
 
 class MySQLTest extends \PHPUnit_Framework_TestCase
 {
+    public function testShouldConnectAndClose()
+    {
+        $link = new \Danilov\Legacy\MySQL('localhost', 'danilov', 'danilov');
+
+        $this->assertInstanceOf(\Danilov\Legacy\MySQL::class, $link);
+
+        $this->assertEquals(0, $link->getErrno());
+        $this->assertEquals('', $link->getError());
+
+        $this->assertTrue($link->close());
+    }
+
+    public function testAccessDenied()
+    {
+        $this->expectException(\PHPUnit_Framework_Error_Warning::class);
+        $this->expectExceptionMessage('(HY000/1045): Access denied for user \'baduser\'@\'localhost\' (using password: YES)');
+
+        new \Danilov\Legacy\MySQL('localhost', 'baduser', 'wrongpassword');
+    }
+
+    public function testAccessDeniedError()
+    {
+        $link = @new \Danilov\Legacy\MySQL('localhost', 'baduser', 'wrongpassword');
+
+        $this->assertEquals(1045, $link->getErrno());
+        $this->assertStringStartsWith('Access denied', $link->getError());
+    }
+
     public function testGetConnection()
     {
         $connection = \Danilov\Legacy\MySQL::getConnection('localhost', 'danilov', 'danilov');
